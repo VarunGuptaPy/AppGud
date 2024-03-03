aac1efc5bb8aa092891a08bc5aadb65b43500cf1
VarunGuptaPy <varun.gupta.py@gmail.com>
2024-03-03 13:57:00 +0530

    Solved some more more x 2 fundamental errors

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
 Color primaryColor = Color(0xff13EBCA);
@@ -14,9 +16,9 @@ Future<void> sendEmail({
   required String firstName,
   required String lastName,
 }) async {
-  String? serviceId = Platform.environment['SERVICE_ID'];
-  String? templateId = Platform.environment['TEMPLATE_ID'];
-  String? userId = Platform.environment['USER_ID'];
+  String? serviceId = dotenv.env['SERVICE_ID'];
+  String? templateId = dotenv.env['TEMPLATE_ID'];
+  String? userId = dotenv.env['USER_ID'];
 
   final url = Uri.parse("https://api.emailjs.com/api/v1.0/email/send");
   final response = await http.post(url,
